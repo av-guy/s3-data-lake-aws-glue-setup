b1ecb3320d12c854db0172d5b682d6ecde109561
av-guy <jonathan@rpi-gpio.dev>
2025-04-28 18:44:41 -0700

    refactor configuration to single file

---
+++ app/config.py
@@ -0,0 +1,17 @@
+"""Application wide configuration variables"""
+
+from configparser import ConfigParser
+from pathlib import Path
+
+config_path = Path(__file__).resolve().parents[1] / "dwh.cfg"
+config = ConfigParser()
+config.read(str(config_path))
+
+S3_BUCKET_NAME = config.get("S3", "S3_BUCKET_NAME")
+ROUTE_TABLE_ID = config.get("EC2", "ROUTE_TABLE_ID")
+VPC_ID = config.get("EC2", "VPC_ID")
+REGION = config.get("AWS", "REGION")
+GLUE_ROLE_NAME = config.get("IAM", "GLUE_ROLE_NAME")
+GLUE_ROLE_POLICY_NAME = config.get("IAM", "GLUE_ROLE_POLICY_NAME")
+S3_ROLE_POLICY_NAME = config.get("IAM", "S3_ROLE_POLICY_NAME")
+
